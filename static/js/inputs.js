986fa9690f3c60d0b3f7d355e64c571b8dfcba22
Shalev Widen <shalevwiden@gmail.com>
2025-10-18 03:48:56 -0500

    Added form code

--- static/js/inputs.js
+++ static/js/inputs.js
@@ -4,15 +4,24 @@ const submitBtn = document.getElementById("submitBtn");
 const blurb = document.querySelector(".blurbifydiv");
 
 submitBtn.addEventListener("click", () => {
-  const name = document.getElementById("nameInput").value;
-  const verified = document.getElementById("verifiedInput").checked;
-  const username = document.getElementById("usernameInput").value;
-  const content = document.getElementById("contentInput").value;
+  const nameinput = document.getElementById("nameInput").value;
+  //   verified is a boolean
+  // make display none on the verified icon if the user selects no for the verification
+
+  const verifiedinput = document.getElementById("verifiedInput").checked;
+  const usernameinput = document.getElementById("usernameInput").value;
+  const contentinput = document.getElementById("contentInput").value;
 
   //   now get the blurb variables
+  const name = blurb.querySelector("#name");
+  const username = blurb.querySelector("#username");
+  const textcontent = blurb.querySelector(".textcontent");
+  //   now change it
   function changeBlurb() {
-    const name = blurb.querySelector("#name");
-    const username = blurb.querySelector("#name");
+    name.innerText = nameinput;
+
+    username.innerText = `@${usernameinput}`;
+    textcontent.innerText = contentinput;
   }
   changeBlurb();
   // Example: dynamically show it
@@ -21,6 +30,11 @@ submitBtn.addEventListener("click", () => {
   //   hmmm
   localStorage.setItem(
     "blurbData",
-    JSON.stringify({ name, verified, username, content })
+    JSON.stringify({
+      name: nameinput,
+      verified: verifiedinput,
+      username: usernameinput,
+      content: contentinput,
+    })
   );
 });
