889694a5eb7251e86cc6bce2e8536414dbd21a6b
Shalev Widen <shalevwiden@gmail.com>
2025-10-18 03:26:38 -0500

    Working on inputs now

--- static/js/inputs.js
+++ static/js/inputs.js
@@ -1,1 +1,26 @@
 // change the content of the Blurb
+
+const submitBtn = document.getElementById("submitBtn");
+const blurb = document.querySelector(".blurbifydiv");
+
+submitBtn.addEventListener("click", () => {
+  const name = document.getElementById("nameInput").value;
+  const verified = document.getElementById("verifiedInput").checked;
+  const username = document.getElementById("usernameInput").value;
+  const content = document.getElementById("contentInput").value;
+
+  //   now get the blurb variables
+  function changeBlurb() {
+    const name = blurb.querySelector("#name");
+    const username = blurb.querySelector("#name");
+  }
+  changeBlurb();
+  // Example: dynamically show it
+
+  // Optional: save it in localStorage
+  //   hmmm
+  localStorage.setItem(
+    "blurbData",
+    JSON.stringify({ name, verified, username, content })
+  );
+});
