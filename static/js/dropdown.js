53878ac68e4a64e24025edf98a0fc26eb2473ea4
Shalev Widen <shalevwiden@gmail.com>
2025-10-23 18:43:00 -0500

    Got the dropdown logic locked

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -4,12 +4,18 @@
 
 // blurbify div is made global in inputs.js
 
+// turns out its relatively simple to implement something like this in JavaScript lmao
+
 function blurb_bg_padding() {
   const circle_down = document.getElementById("circle_down");
 
   circle_down.addEventListener("click", () => {
     console.log("circle down circle clicked");
+
+    // classes are defined in _dropdown.scss
+
     blurbify_bg_div.classList.toggle("no_top_padding");
+    circle_down.classList.toggle("rotated");
   });
 }
 blurb_bg_padding();
