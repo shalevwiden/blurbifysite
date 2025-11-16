45e140a5adb096d8efba3e2851d77fb59d8c553a
Shalev Widen <shalevwiden@gmail.com>
2025-11-16 03:43:58 -0600

    Added navbar click js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -8,3 +8,16 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("hidden");
   }
 });
+const navbar = document.querySelector(".navbar");
+
+const ul = navbar.querySelector("ul");
+const lis = ul.querySelectorAll("li");
+
+lis.forEach((li) => {
+  const link = li.querySelector("a");
+  if (!link) return; // skip malformed items
+
+  li.addEventListener("click", () => {
+    window.open(link.href, "_self");
+  });
+});
